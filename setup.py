48dc553181614fe6a0e11b04a768ac5c2eefc9e0
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-02-12 19:26:56 +0200

    lib.sh

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'bash-plus', version = '0.3',
+    name = 'bash-plus', version = '0.4',
     url = 'https://www/github.com/moshloop/bash-plus',
     author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com',
     scripts = ['bash-plus']
